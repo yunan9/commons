f12ca473adc5061d2b2ea43d56b00c83ad90ca73
yunan9 <itzcollor@gmail.com>
2025-11-13 16:02:43 -0500

    chore: rm dependencies and repo declaration

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,14 +24,6 @@ tasks.withType<Javadoc>().configureEach {
     }
 }
 
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    api("org.jetbrains:annotations:26.0.2-1")
-}
-
 publishing {
     publications {
         create<MavenPublication>("maven") {
