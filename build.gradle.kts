c9b56c6ffda4efe001f29ffb4a80707e1dcad2bf
yunan9 <itzcollor@gmail.com>
2025-11-13 00:42:36 -0500

    feat: setup maven central publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,59 @@ plugins {
     `maven-publish`
 }
 
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+
+    withJavadocJar()
+    withSourcesJar()
+}
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
     api("org.jetbrains:annotations:26.0.2-1")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+
+            val githubRepository = System.getenv("GITHUB_REPOSITORY")
+            val githubRepositoryUrl = "https://github.com/$githubRepository"
+            pom {
+                url = githubRepositoryUrl
+                name = project.name
+                description = project.description
+
+                developers {
+                    developer {
+                        id = "yunan9"
+                        name = "Yunan"
+                    }
+                }
+
+                scm {
+                    url = githubRepositoryUrl
+                    connection = "scm:git:$githubRepositoryUrl.git"
+                    developerConnection = "scm:git:ssh://git@github.com/$githubRepository.git"
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+
+            credentials {
+                username = System.getenv("OSSRH_USERNAME")
+                password = System.getenv("OSSRH_TOKEN")
+            }
+        }
+    }
 }
